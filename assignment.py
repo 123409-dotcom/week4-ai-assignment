59d8852fbed46e5c8568557171fa824d94c1d0ff
Joseph Juma <jj4443193@gmail.com>
2026-03-11 11:48:32 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,13 @@
 # Question 4
+Write a python program that prints the following message 
+Hello AI Students
+print("Hello AI Students")
 
 # Question 5
+Create the following variables in python:
+Name="joseph"
+Age="21"
+City="Kisumu"
+print(Name+Age+City)
+
+# Question 6
